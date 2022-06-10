e8185c9fc609ae8aeb0ec344f630500c265a1f29
believemaster <believemasters@gmail.com>
2022-06-10 23:14:57 +0530

    Setting up restful microservice with DB

---
+++ RestfulMicroServiceWithDB/src/main/java/com/yanik/RestfulMicroServiceWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.yanik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulMicroServiceWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulMicroServiceWithDbApplication.class, args);
+	}
+
+}
