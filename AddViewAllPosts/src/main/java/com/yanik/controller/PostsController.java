c1edd79c50cb610e56af35e6f2854315c6184c5c
believemaster <believemasters@gmail.com>
2022-06-10 19:43:39 +0530

    Show all posts function

---
+++ AddViewAllPosts/src/main/java/com/yanik/controller/PostsController.java
@@ -0,0 +1,21 @@
+package com.yanik.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yanik.entity.Post;
+import com.yanik.services.PostsService;
+
+@RestController
+public class PostsController {
+
+//	By default spring boot convert list into JSON response on localhost:8080/posts
+	@RequestMapping("/posts")
+	public List<Post> getPosts() {
+		return new PostsService().getPosts();
+	}
+	
+}
---
+++ AddViewAllPosts/src/main/java/com/yanik/services/PostsService.java
@@ -0,0 +1,27 @@
+package com.yanik.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.yanik.entity.Post;
+
+@Service
+public class PostsService {
+	
+	
+	List<Post> posts = new ArrayList<>();
+
+	public PostsService() {
+		posts.add(new Post(1, "Datatype", "Sample Txt"));
+		posts.add(new Post(2, "Classes", "Sample Txt"));
+		posts.add(new Post(3, "Object", "Sample Txt"));
+		posts.add(new Post(4, "OOPS concepts", "Sample Txt"));
+	}
+	
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+}
